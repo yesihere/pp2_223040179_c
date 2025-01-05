514eae274012e5b011535c3b0c25c9312ac4c931
yesihere <dedehidayatiyesi@gmai;.com>
2025-01-05 22:42:23 +0700

    pert5

---
+++ src/pert5/JTableSortExample.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pert5;
+
+/**
+ *
+ * @author DELL
+ */
+import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+public class JTableSortExample {
+     public static void main(String[] args) {
+ JFrame frame = new JFrame("JTable Sorting Example");
+ String[] columnNames = { "ID", "Name", "Age" };
+ Object[][] data = {
+ { 1, "John", 25 },
+ { 3, "Mike", 35 },
+ { 2, "Anna", 30 },
+ };
+ DefaultTableModel model = new DefaultTableModel(data, columnNames);
+ JTable table = new JTable(model);
+ // Mengaktifkan pengurutan otomatis
+ table.setAutoCreateRowSorter(true);
+    JScrollPane scrollPane = new JScrollPane(table);
+ frame.add(scrollPane);
+ frame.setSize(400, 200);
+ frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+ frame.setVisible(true);
+ }
+}
